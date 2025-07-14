aa83d7772549e3a3c7a0bee73e7e1dd61dab4e1f
BloomAT <atsbloom@protonmail.com>
2025-07-14 14:49:27 -0700

    Updated file

--- Module0ReviewPythonPractice/Module 0 Homework.py
+++ Module0ReviewPythonPractice/Module 0 Homework.py
@@ -2,5 +2,5 @@ Python 3.12.4 (v3.12.4:8e8a4baf65, Jun  6 2024, 17:33:18) [Clang 13.0.0 (clang-1
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> print("CSC 142")
 CSC 142
->>> print("I learned how to install gitand use through terminal")
-I learned how to install gitand use through terminal
+>>> print("I learned that it sucks trying to pull files through to GitHub with terminal on mac")
+I learned that it sucks trying to pull files through to GitHub with terminal on mac
