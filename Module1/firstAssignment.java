07b2315fc5dc537f876c2246fa37f3d637627a28
BloomAT <atsbloom@protonmail.com>
2025-08-01 14:04:12 -0700

    Updated

---
+++ Module1/firstAssignment.java
@@ -0,0 +1,15 @@
+package lab0;
+
+public class firstAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("CSC142");
+		System.out.println("I learned that quotes and apostrophe's cannot be used interchangibly in Java like they can in Python.");
+		
+		
+
+	}
+
+}
